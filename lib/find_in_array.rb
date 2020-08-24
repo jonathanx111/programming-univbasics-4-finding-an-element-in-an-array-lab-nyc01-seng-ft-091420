27017558a785330bdcf54d21adfc20a3f086a046
Jonathan Xiao <jonathanx111@gmail.com>
2020-08-24 04:59:39 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,1 @@
-
-def find_element_index(array, value_to_find)
-  array.length.times { |x|
-    if x == value_to_find
-      return array.index(value_to_find)
-    end
-  }
-end
+def find_element(array, value_to_find)
