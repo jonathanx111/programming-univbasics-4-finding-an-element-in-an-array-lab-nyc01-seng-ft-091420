6e3220b5d3e86fe17016b5e047397361153d68a7
Jonathan Xiao <jonathanx111@gmail.com>
2020-08-24 05:01:09 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,1 +1,10 @@
 def find_element(array, value_to_find)
+  count = 0 
+  while count < array.length do
+    if array[count] == value_to_find
+      return count
+    end
+    
+    count += 1
+  end
+end
